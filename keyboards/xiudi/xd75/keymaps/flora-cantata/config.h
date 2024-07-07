d007a8eb580c8f7b465ea38edd509ec423b6d2f2
purple-rw <purple-rw@yahoo.com>
2024-07-07 15:12:50 -0700

    add XD75 keymap for Flora

---
+++ keyboards/xiudi/xd75/keymaps/flora-cantata/config.h
@@ -0,0 +1,9 @@
+/* tri layers */
+#define TRI_LAYER_LOWER_LAYER  3    // _RAISE
+#define TRI_LAYER_UPPER_LAYER  5    // _LOWER
+#define TRI_LAYER_ADJUST_LAYER 7    // _MODS
+
+#ifdef VIA_ENABLE
+/* VIA configuration. */
+    #define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#endif // VIA_ENABLE
---
+++ keyboards/xiudi/xd75/keymaps/flora-cantata/readme.md
@@ -0,0 +1,3 @@
+# The keymap for xd75
+
+Main user uses Qwerty layout, but secondary user may use Colmak DH.
